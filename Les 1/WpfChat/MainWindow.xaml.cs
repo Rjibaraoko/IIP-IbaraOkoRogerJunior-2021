33c4a1da4bdbe7d84b9d01ec228112db479fe034
Rjibaraoko <junior4553@hotmail.fr>
2020-10-31 17:07:47 +0100

    update wpfchat

---
+++ Les 1/WpfChat/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfChat
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+             
+            Chatlbl.Content = Naam1.Text + Environment.NewLine + Berichttxt.Text;
+            Berichttxt.Text = "";
+            Naam1.Text = "";
+
+            /*String i = "pussy";
+            String ii = "fuck";
+            String iii = "ass";
+
+            i = i.Replace("Pussy", "*****");
+            ii = ii.Replace("Fuck", "*****");
+            iii = iii.Replace("ass", "*****");*/
+
+
+        }
+
+    }
+}
