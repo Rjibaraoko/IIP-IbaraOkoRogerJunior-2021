6ad0e017a57b6ea63d06e427a641cc2e6d9809c8
Rjibaraoko <junior4553@hotmail.fr>
2020-11-01 16:13:32 +0100

    les 2 start

---
+++ Les 2/ConsoleBMI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //De tekst genereren
+            Console.WriteLine("BMI CALCULATOR");
+            Console.WriteLine("==============");
+            Console.WriteLine("Lengte (in cm):");
+            //De input van de user onthouden
+            var Lengte = Convert.ToDouble(Console.ReadLine()) / 100;
+            Console.WriteLine("Gewicht (in kg):");
+            var Gewicht = Convert.ToDouble(Console.ReadLine());
+            //formule voor het maken van de berekening van de BMI
+            var BMI = Gewicht / (Lengte * Lengte);
+            //het resultaat 
+            Console.WriteLine("Je BMI bedraagt:" + Math.Ceiling(BMI));
+
+            Console.ReadKey();
+            
+        }
+    }
+}
