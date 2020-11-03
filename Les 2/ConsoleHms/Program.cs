292016089f5c5a3a115a7042135f6df4772f9158
Rjibaraoko <junior4553@hotmail.fr>
2020-11-03 16:29:52 +0100

    Update Program.cs

--- Les 2/ConsoleHms/Program.cs
+++ Les 2/ConsoleHms/Program.cs
@@ -16,11 +16,9 @@ namespace ConsoleHms
 
 
             TimeSpan t = TimeSpan.FromSeconds(input);
+
             //D2, D3 zijn het aantal digits in de conversie van seconden naar een echt uur
-            string answer = string.Format("{0:D2}h:{1:D2}m:{2:D2}s",
-                            t.Hours,
-                            t.Minutes,
-                            t.Seconds);
+            string answer = string.Format("{0:D2}h:{1:D2}m:{2:D2}s", t.Hours, t.Minutes,t.Seconds);
             Console.WriteLine("Omgezet in hms formaat:" + answer);
             Console.Read();
         }
