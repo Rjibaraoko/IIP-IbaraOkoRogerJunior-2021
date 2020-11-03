8c89c55350eca49292e4e2c7aba803883acbef13
Rjibaraoko <junior4553@hotmail.fr>
2020-11-03 16:16:45 +0100

    up

---
+++ Les 2/ConsoleHms/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Eerste zin d
+            Console.WriteLine("Geef het aantal seconden:");
+            var input = Convert.ToInt32(Console.ReadLine());
+
+
+            TimeSpan t = TimeSpan.FromSeconds(input);
+            //D2, D3 zijn het aantal digits in de conversie van seconden naar een echt uur
+            string answer = string.Format("{0:D2}h:{1:D2}m:{2:D2}s",
+                            t.Hours,
+                            t.Minutes,
+                            t.Seconds);
+            Console.WriteLine("Omgezet in hms formaat:" + answer);
+            Console.Read();
+        }
+    }
+}
---
+++ Les 2/ConsoleKarakter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleKarakter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geef een kleine letter:");
+            var input = Console.ReadLine();
+            string[] alphabet = { "a", "b", "c", "d", "e", "f", "g", "h", 
+            "i", "j", "k", "l", "m","n", "o", "p", "q", "r", "s", "t", "u", 
+            "v", "w", "x", "y","z"};
+
+            if (input == "")
+            {
+                alphabet[]
+            }
+
+            Console.WriteLine("De hoofdletter is:" + input.ToUpper());
+            Console.WriteLine("De vorige letter is:" + input.());
+            Console.WriteLine("De volgende letter is:" + input.ToUpper());
+            Console.Read();
+
+        }
+    }
+}
