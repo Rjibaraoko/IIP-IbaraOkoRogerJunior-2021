532496dfaf607d69ed04279157d5468729ec956d
Rjibaraoko <junior4553@hotmail.fr>
2020-11-06 16:31:16 +0100

    Fase 2 75 procent done

--- Project/Project Ibara-Oko Roger Junior/ConsoleMusicPlayer/Program.cs
+++ Project/Project Ibara-Oko Roger Junior/ConsoleMusicPlayer/Program.cs
@@ -13,13 +13,20 @@ namespace ConsoleMusicPlayer
     {
         static void Main(string[] args)
         {
+
             //de tekst dat zal opkomen bij het starten van het programma
             Console.WriteLine("MEDIAPLAYER");
             Console.WriteLine("===========");
             Console.WriteLine("Bestand afspelen:");
             //Wat de user zal inzetten als path
             var input = Console.ReadLine();
-
+            Console.ReadKey();
+            Console.WriteLine("Spacebar om te pauseren/play");
+            Console.WriteLine("Uparrow om te muten");
+            Console.WriteLine("Downarrow om te ontmuten");
+            Console.WriteLine("Enter om liedje te stoppen");
+            Console.WriteLine("Backspace om geluid te veranderen");
+            
             //What zal het zoeken van de file in het systeem zorgen
             //System.Diagnostics.Process.Start(input);
 
@@ -41,47 +48,71 @@ namespace ConsoleMusicPlayer
             string path = input;
             FileInfo filepath = new FileInfo(path);
 
+            int druk = Convert.ToInt32(keypress.Key);
+            druk = 0;
 
-            /*while (File.Exists(input))
+            
+            while (keypress.Key == ConsoleKey.Spacebar)
             {
-                if (keypress.Key == ConsoleKey.UpArrow)
-                {
-                    player.controls.pause();
-                }
-                else if (keypress.Key == ConsoleKey.Spacebar)
+                
+                player.controls.pause();
+                druk++;
+
+
+                if (keypress.Key == ConsoleKey.Spacebar && druk > 1)
                 {
                     player.controls.play();
+                    druk--;
+                    
                 }
-            }*/
 
-            bool rap = Convert.ToBoolean(keypress.Key);
+            }
 
-            while (rap)
+            while (keypress.Key == ConsoleKey.Enter)
             {
-                switch (keypress.Key)
+                player.controls.stop();
+                break;
+            }
+
+            if (keypress.Key == ConsoleKey.Backspace)
+            {
+                int huidigVolume = player.settings.volume;
+                Console.WriteLine($"Huidig volume: {huidigVolume}");
+
+                Console.WriteLine("Naar hoeveel wil je het volume veranderen?");
+                player.settings.volume = Convert.ToInt32(Console.ReadLine());
+
+                //int Volumes = Convert.ToInt32(Console.ReadLine());
+                /*if (Volumes > 100 )
                 {
-                    case ConsoleKey.Spacebar:
-                        {
-                            player.controls.pause();
-                            break;
-                        }
-                    case ConsoleKey.Escape:
-                        {
-                            player.controls.play();
-                            break;
-                        }
-                    case ConsoleKey.Enter:
-                        {
-                            player.controls.stop();
-                            break;
-                        }
-                    default: break;
+                    Console.WriteLine("Ongeldige waarde. Geef een waarde onder de 100");
                 }
+
+                if (Volumes < 0)
+                {
+                    Console.WriteLine("Ongeldige waarde. Geef een waarde boven de 0");
+                }*/
+
+
+                  }
+
+            //als je op de up arrow drkt zal de app de volume helemaal muten
+            if (keypress.Key == ConsoleKey.UpArrow)
+            {
+                player.settings.mute = true;
+            }
+            //als je op de up arrow drukt zal de applicatie de volume terug herstellen
+            if (keypress.Key == ConsoleKey.DownArrow)
+            {
+                player.settings.mute = false;
             }
 
             
 
-            Console.ReadKey();
+
+            Console.Read();
+
+
             
         }
     }
