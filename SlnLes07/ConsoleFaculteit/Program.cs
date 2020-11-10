ac4165d4b99103248d24af221f11a4cdd9d3c301
Rjibaraoko <junior4553@hotmail.fr>
2020-11-10 23:37:23 +0100

    khb

---
+++ SlnLes07/ConsoleFaculteit/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFaculteit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FACULTEIT BEREKENEN");
+            Console.WriteLine("Geef een geheel getal:");
+            int getal = Convert.ToInt32(Console.ReadLine());
+            Faculteit(getal);
+            Console.ReadKey();
+        }
+
+        private static int Faculteit(int n)
+        {
+            for (int i = n-1; i <= n; i--)
+            {
+                int resultaat = i * i;
+                Console.WriteLine($"de faculteit is {resultaat}");
+                
+
+            }
+            return n;
+
+        }
+    }
+}
