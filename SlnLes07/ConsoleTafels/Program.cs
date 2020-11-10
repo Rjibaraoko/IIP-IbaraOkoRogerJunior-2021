0f49c267c5052a6386ff4a8c5867c6b02d196f8d
Rjibaraoko <junior4553@hotmail.fr>
2020-11-10 21:25:56 +0100

    opgave 1 done

---
+++ SlnLes07/ConsoleTafels/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTafels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geef een getal:");
+            int get1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Geef een lengte:");
+            int get2 = Convert.ToInt32(Console.ReadLine());
+            DrukTafel(get1, get2);
+            Console.ReadKey();
+        }
+
+        private static int DrukTafel(int getal, int lengte)
+        {
+            int result = getal * lengte;
+            for (int i = 1; i <= lengte; i++)
+            {
+                int resultaat = getal * i;
+                Console.WriteLine($"{getal} X {i} = {resultaat}");
+            }
+            return result;
+            
+        }
+    }
+}
