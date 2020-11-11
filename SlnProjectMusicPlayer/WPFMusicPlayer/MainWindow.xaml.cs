8a91d6740cc089dd5231276eb132a098550319e7
Rjibaraoko <junior4553@hotmail.fr>
2020-11-11 17:16:07 +0100

    pi

---
+++ SlnProjectMusicPlayer/WPFMusicPlayer/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WMPLib;
+
+namespace WPFMusicPlayer
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowsMediaPlayer player = new WindowsMediaPlayer();
+
+            //De path naar de folder van de muzieken
+            string musicFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+
+            string input = Convert.ToString(listbx.SelectedItem);
+
+            //De exacte path naar het liedje met de naam                         
+
+            player.URL = System.IO.Path.Combine(musicFolder, input);
+        }
+
+        private void PLAY_Click(object sender, RoutedEventArgs e)
+        {
+            player.controls.play();
+
+        }
+    }
+}
