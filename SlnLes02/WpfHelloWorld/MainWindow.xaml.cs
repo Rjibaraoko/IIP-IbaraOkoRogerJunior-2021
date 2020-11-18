8f423eff0f520b6040fa7384863b43bb13f7834c
Rjibaraoko <junior4553@hotmail.fr>
2020-11-18 19:52:03 +0100

    les 2 done

---
+++ SlnLes02/WpfHelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfHelloWorld
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            Hallolbl.Content = "Hallo " + Inputtxt.Text;
+            
+        }
+
+        private void HERSTEL_Click(object sender, RoutedEventArgs e)
+        {
+            Hallolbl.Content = "Hallo, hoe is je naam?";
+            Inputtxt.Text = " ";
+        }
+    }
+}
