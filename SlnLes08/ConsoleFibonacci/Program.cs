fa944dda4adb23b0ce74e151d4830be81302d4fb
Rjibaraoko <junior4553@hotmail.fr>
2020-11-20 19:31:18 +0100

    les 8fs

--- SlnLes08/ConsoleFibonacci/Program.cs
+++ SlnLes08/ConsoleFibonacci/Program.cs
@@ -12,11 +12,14 @@ namespace ConsoleFibonacci
         {
             Console.WriteLine("Geef een cijfer:");
             int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n");
+            // het werkt alleen maar als ik een var gebruik doordat het een array is. Ik weet niet precies waarom....
             var x = Fibonacci(input);
 
             foreach (int nummers in x)
             {
                 Console.WriteLine(nummers);
+                
             }
             
             Console.Read();
@@ -24,15 +27,16 @@ namespace ConsoleFibonacci
 
         private static int[] Fibonacci(int nummer)
         {
-            
-            int[] a = new int[nummer];
-            a[0] = 0;
-            a[1] = 1;
+            int[] y = new int[nummer];
+            y[0] = 0;
+            y[1] = 1;
+
             for (int i = 2; i < nummer; i++)
             {
-                a[i] = a[i - 2] + a[i - 1];
+                y[i] = y[i - 2] + y[i - 1];
             }
-            return a;
+
+            return y;
         }
     }
     
